fe97601d60f9e2ee1cd8927146c45680b29e321b
Bob Costas <finkzane@gmail.com>
2018-03-11 14:04:12 -0700

    Added testing for iteratorClassLL

---
+++ IteratorClassLLMain.java
@@ -0,0 +1,14 @@
+package p6_Package;
+
+public class IteratorClassLLMain
+{
+    public static void main( String[] args )
+    {
+        IteratorClassLL ll = new IteratorClassLL();
+
+        ll.insertAfterIterator( 4 );
+        ll.insertAfterIterator( 5 );
+        ll.insertAfterIterator( 6 );
+        ll.insertAfterIterator( 7 );
+    }
+}
