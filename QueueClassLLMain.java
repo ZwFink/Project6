015606dc3a602ea21aff9aa8c02ea8accaf0045a
Bob Costas <finkzane@gmail.com>
2018-03-12 19:21:45 -0700

    More testing for queue

--- QueueClassLLMain.java
+++ QueueClassLLMain.java
@@ -1,4 +1,30 @@
 package p6_Package;
 
-public class QueueClassLLMain {
+public class QueueClassLLMain
+{
+    public static void main( String [] args )
+    {
+        QueueClassLL que = new QueueClassLL();
+
+        for( int index = 0; index < 5; index++ )
+        {
+            que.enqueue( index );
+        }
+
+        QueueClassLL que2 = new QueueClassLL( que );
+
+        System.out.println( que );
+        System.out.println( que2 );
+
+        que.dequeue();
+        que.dequeue();
+        que.dequeue();
+        que.dequeue();
+        que.dequeue();
+
+        System.out.println(que.peekFront());
+        System.out.println( que );
+
+
+    }
 }
