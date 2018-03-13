d5e4eacf2dc4831edc31e13a48e001443614de0f
Bob Costas <finkzane@gmail.com>
2018-03-12 19:16:14 -0700

    Fixed bug where peekFront did not set current iterator position to the end

--- QueueClassLL.java
+++ QueueClassLL.java
@@ -54,7 +54,8 @@ public class QueueClassLL
      */
    public int peekFront()
    {
-       return queueData.retrieveAtCurrent()
+       queueData.setToEnd();
+       return queueData.retrieveAtCurrent();
    }
 
     /**
