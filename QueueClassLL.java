9aaf66abdd26ce035bf448b1b38ad5747de8e97a
Bob Costas <finkzane@gmail.com>
2018-03-12 19:07:25 -0700

    Added default and copy constructors

--- QueueClassLL.java
+++ QueueClassLL.java
@@ -1,8 +1,31 @@
 package p6_Package;
 
+
 /**
  * Class manages data in queue form, using IteratorClass
  * @author Zane Fink
  */
-public class QueueClassLL {
+public class QueueClassLL
+{
+   /**
+    * queue data managed by IteratorClass object
+    */
+   IteratorClassLL queueData;
+
+    /**
+     * Default constructor
+     */
+   public QueueClassLL()
+   {
+      queueData = new IteratorClassLL();
+   }
+
+    /**
+     * Copies a queueClass object
+     * @param copied QueueClass object to be copied
+     */
+   public QueueClassLL( QueueClassLL copied )
+   {
+       queueData = new IteratorClassLL( copied.queueData );
+   }
 }
