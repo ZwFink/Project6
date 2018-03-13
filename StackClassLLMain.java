05b03051e1174cb155393da503d6200a4cf74961
Bob Costas <finkzane@gmail.com>
2018-03-12 18:53:33 -0700

    Added testing for push/pop methods

--- StackClassLLMain.java
+++ StackClassLLMain.java
@@ -2,5 +2,16 @@ package p6_Package;
 
 public class StackClassLLMain
 {
-    
+    public static void main( String[] args )
+    {
+        StackClassLL stack = new StackClassLL();
+
+        stack.push( 1 );
+        stack.push( 2 );
+        stack.push( 3 );
+
+        System.out.println( stack.toString() );
+        stack.pop();
+        System.out.println( stack.toString() );
+    }
 }
